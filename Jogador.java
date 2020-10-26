c69ab48e65ecb1c77bad5a28dc6de125b08f638f
Filipe Viana <filipe.viana@ufv.br>
2020-10-26 16:57:25 -0300

    Classe jogador init

---
+++ Jogador.java
@@ -0,0 +1,38 @@
+public class Jogador{
+    public Jogador(short atk, short def){
+        this.atk = atk;
+        this.def = def;
+        this.y = 3;
+        this.x = 3;
+    }
+    private short atk,def,x,y;
+    
+    public short getAtk(){
+        return this.atk;
+    }
+    public short getDef(){
+        return this.atk;
+    }
+    public short getX(){
+        return this.x;
+    }
+    public short getY(){
+        return this.y;
+    }
+
+    public void setAtk(short atk){
+        this.atk = atk;
+    }
+    public void setDef(short def){
+        this.atk = def;
+    }
+    public void setX(short x){
+        this.x = x;
+    }
+    public void setY(short y){
+        this.y = y;
+    }
+    public void setAtk(short atk){
+        this.atk = atk;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+## Projeto Final SIN-141 - Programa Orientada a Objetos
+
+Antivírus por um dia é um jogo de tabuleiro desktop colaborativo projetado para dois
+jogadores. Esse jogo foi inventado por Clausius Reis e tem como objetivo percorrer os
+setores da memória de um computador infectados por vírus. O jogador assume o papel de
+antivírus, que deve avançar pelos setores, eliminando os vírus que encontrar, até que o
+setor com a fonte de infecção seja alcançado. Mas tome cuidado, os vírus podem te destruir
+no caminho.
