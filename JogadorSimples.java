ecd17d3d768875573e03583659b2b314159c4397
Filipe Viana <filipe.viana@ufv.br>
2020-11-11 11:52:22 -0300

    Subclasse jogador simples e jogador suporte

---
+++ JogadorSimples.java
@@ -0,0 +1,11 @@
+public class JogadorSimples extends Jogador{
+    public JogadorSimples(){
+        super(2,6);
+    }
+    public void setAtk(short atk){
+        this.atk = atk;
+    }
+    public void setDef(short def){
+        this.atk = def;
+    }
+}
---
+++ JogadorSuporte.java
@@ -0,0 +1,14 @@
+public class JogadorSimples extends Jogador{
+    public JogadorSimples(){
+        super(2,6);
+    }
+    public void setAtk(short atk){
+        this.atk = atk;
+    }
+    public void setDef(short def){
+        this.atk = def;
+    }
+    public void recuperarDefesa(){
+        //Implementar método
+    }
+}
