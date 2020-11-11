f37192773add3ddcc17fdc3812bc8121eeeba46e
Filipe Viana <filipe.viana@ufv.br>
2020-11-11 11:40:31 -0300

    classe Inimigo

---
+++ Inimigo.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author Gabriel Lopes
+ */
+public class Inimigo {
+    public Inimigo(int atk, int def){
+        this.atk = atk;
+        this.def = def;
+    }
+    private int atk, def;
+
+    public int getAtk() {
+        return atk;
+    }
+    public void setAtk(short atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+    public void setDef(short def) {
+        this.def = def;
+    }
+    
+    public void gerarValores(){
+        Random gerador = new Random();
+        for(int i = 1; i<2; i++){
+            atk=gerador.nextInt(4);
+            def=gerador.nextInt(4);
+        }
+    }
+}
