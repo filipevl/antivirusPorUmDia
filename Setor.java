d6b7c095c6c33ead2d2cde59a6be258b15460479
Filipe Viana <filipe.viana@ufv.br>
2020-11-11 13:32:19 -0300

    correções de sintaxe

--- Setor.java
+++ Setor.java
@@ -1,11 +1,12 @@
 public class Setor{
     public Setor(boolean primeiroLado,boolean segundoLado,boolean terceiroLado,boolean quartoLado, boolean fonteDeInfeccao){
-        this.primeiroLado=primeiroLado,
-        this.segundoLado=segundoLado,
-        this.terceiroLado=terceiroLado,
-        this.quartoLado=quartoLado,
+        this.primeiroLado=primeiroLado;
+        this.segundoLado=segundoLado;
+        this.terceiroLado=terceiroLado;
+        this.quartoLado=quartoLado;
         this.fonteDeInfeccao=fonteDeInfeccao;
     }
+    public Setor(){}
     private boolean primeiroLado, segundoLado, terceiroLado, quartoLado,fonteDeInfeccao;
 
     public boolean getFonteDeInfeccao(){
--- Tabuleiro.java
+++ Tabuleiro.java
@@ -2,18 +2,18 @@ public class Tabuleiro{
     public Tabuleiro(){
     }
     private Setor[][] posicao = new Setor[4][4];
-    public void inicializa(posicao){
+    public void inicializa(Setor[][] posicao){
         for(short x=0;x<5;x++){
             for(short y=0;y<5;y++){
                 if(x==3&&y==3){
-                    posicao[x][y] = newSetor(true,true,true,true,false); //Posição C
+                    posicao[x][y] = new Setor(true,true,true,true,false); //Posição C
                 }
-                posicao[x][y] = newSetor();
+                posicao[x][y] = new Setor();
             }
         }
     }
 
-    public Setor getPosicao(){
+    public Setor[][] getPosicao(){
         return posicao;
     }
 }
