ce7ec776e6b2ffc683ebd8192d057ce20fe622cd
Filipe Viana <filipe.viana@ufv.br>
2020-11-17 19:26:19 -0300

    método atacar do inimigo

--- Inimigo.java
+++ Inimigo.java
@@ -10,10 +10,6 @@ public class Inimigo {
     public int getAtk() {
         return atk;
     }
-    public void setAtk(int atk) {
-        this.atk = atk;
-    }
-
     public int getDef() {
         return def;
     }
@@ -30,7 +26,21 @@ public class Inimigo {
         atk=gerador.nextInt(4);
         def=gerador.nextInt(4);
     }
-    public void atacar(){
-
+    public void atacar(Jogador j1,Jogador j2){
+        Random gerador = new Random();
+        int num = gerador.nextInt(7);
+        if(num==0){
+            num += 1;
+        }
+        if(num%2==0){
+            if(j1.getX()==getX()&&j1.getY()==getY()){
+                j1.setDef(j1.getDef()-getAtk());
+            }
+            if(j2.getX()==getX()&&j2.getY()==getY()){
+                j2.setDef(j2.getDef()-getAtk());
+            }
+        }else{
+            return;
+        }
     }
 }
